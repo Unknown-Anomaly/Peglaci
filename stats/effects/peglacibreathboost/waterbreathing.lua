9c730fea703fdbfc7b5a018882b21389512c6a49
Gonzalo Romero <gondragon13@gmail.com>
2017-09-26 15:14:24 -0300

    Merged CrypticXenon and GonDragon branches

--- stats/effects/peglacibreathboost/waterbreathing.lua
+++ stats/effects/peglacibreathboost/waterbreathing.lua
@@ -1,6 +1,8 @@
 function init()
   effect.addStatModifierGroup({
+    {stat = "breathProtection", amount = 1},
     {stat = "waterImmunity", amount = 1},
+    {stat = "wetImmunity", amount = 1}
   })
 end
 
