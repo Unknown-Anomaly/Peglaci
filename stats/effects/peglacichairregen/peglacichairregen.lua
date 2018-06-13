5dfebb4086d377101cb66c1f28d96025e6a2bbe5
HALLEYSHURA <35978759+HALLEYSHURA@users.noreply.github.com>
2018-06-13 05:07:20 -0400

    ship chair healing reimplementation + bed fixes
    dunno if you guys wanna keep this, but this was mentioned in the original mod description on the SB site, figured i'd try to reimplment it.
    
    the ship chair in peglaci ships now provides regeneration when sat in. it also stops hunger, similar to the bed resting effect. UNLIKE the resting effect, it won't strip you naked when you sit in it :)
    
    seems to work fine, but again, up to you guys if you wish to keep it or tweak the regen rate. at the moment, it only uses t1 healing.
    
    additionally, peglaci beds now provide the resting buff instead of both regen and nude. resting p much has both these effects, but might as well have it for the sake of consistency with vanilla beds.

---
+++ stats/effects/peglacichairregen/peglacichairregen.lua
@@ -0,0 +1,20 @@
+require "/scripts/rect.lua"
+
+function init()
+  effect.addStatModifierGroup({stat = "foodDelta", effectiveMultiplier = 0})
+  if status.isResource("food") and not status.resourcePositive("food") then
+    status.setResource("food", 0.01)
+  end
+
+  script.setUpdateDelta(5)
+
+  self.healingRate = 1.0 / config.getParameter("healTime", 60)
+end
+
+function update(dt)
+  status.modifyResourcePercentage("health", self.healingRate * dt)
+end
+
+function uninit()
+
+end
